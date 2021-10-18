c9e18d80d708fcc6201ae54fb1eb65f05cd6c306
shuvy <shuvy@500tech.com>
2021-10-19 00:03:09 +0300

    Change api-url in prod

--- src/constants/chat.constants.js
+++ src/constants/chat.constants.js
@@ -3,7 +3,7 @@ export const ACCESS_TOKEN = "access_token";
 const isProduction = process.env.NODE_ENV === "production";
 
 export const BASE_API_URL = isProduction
-  ? process.env.REACT_APP_BASE_API_URL_PROD
+  ? ''
   : process.env.REACT_APP_BASE_API_URL_DEV;
 
 export const MEDIA_QUERY_MOBILE = 600;
