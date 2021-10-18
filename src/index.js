d7027647bcc96aad2b4d8d75933bdf4c275e0fdc
shuvy <shuvy@500tech.com>
2021-10-18 22:23:45 +0300

    Remove log index js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-console.log(process.env);
 ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider
